288a3f38359a49dd0a2011dc806785f125021c9c
Swapnil <swapnil@MacBook-Pro-swapnilzarbade.local>
2018-04-08 18:59:34 +0530

    added path /home/jenkins/workspace/rpm-build-test/rpm-build/

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ RPM_BUILD_DIR="/home/jenkins/rpmbuild/BUILD"
 RPM_BUILD_ROOT="/home/jenkins/rpmbuild/BUILDROOT/${pkg_name}-${release}.x86_64"
 
 export RPM_SOURCE_DIR RPM_BUILD_DIR RPM_BUILD_ROOT
-
-tar -czvf ${pkg_name}.tar.gz /home/jenkins/workspace/rpm-build-test/rpm-build/${pkg_name}
+cd /home/jenkins/workspace/rpm-build-test/rpm-build/
+tar -czvf ${pkg_name}.tar.gz ${pkg_name}
 cp ${pkg_name}.tar.gz $RPM_SOURCE_DIR
 
 rpmbuild -ba --define "release $release" rpm-build/$spec_file

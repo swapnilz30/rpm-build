5b77c0302f5ada83830467912866c51eab2d9581
Swapnil <swapnil@MacBook-Pro-swapnilzarbade.local>
2018-04-13 20:03:24 +0530

    added s

--- copy_files/build.sh
+++ copy_files/build.sh
@@ -2,7 +2,7 @@
 
 spec_file=copy_file.spec
 
-cd /home/jenkins/workspace/copy_file/rpm-build/copy_files
+cd /home/jenkins/workspace/copy_files/rpm-build/copy_files
 while getopts b: name
 do
 	case $name in
